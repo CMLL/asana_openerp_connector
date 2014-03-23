c2d52c5da93b6b868189847bb5c782a4c10ef202
Carlos Llamacho <llamacholancho@gmail.com>
2014-03-23 19:50:29 -0400

    Fixed syntax error at asana.connector model.

--- asana_connector.py
+++ asana_connector.py
@@ -11,7 +11,8 @@ class AsanaConnector(orm.Model):
     _columns = {
         'name': fields.char('User Name', size=9, help='Your Asana username.'),
         'api_key': fields.char('API Key', size=32, required=True, help="""Your Asana Account API key,
-                               you can generate this key in your settings page of Asana.""")
+                               you can generate this key in your settings page of Asana."""),
+        'state': fields.selection((('draft', 'Draft'), ('connected', 'Connected')), 'State'),
     }
 
 

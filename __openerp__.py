7e014b958381a008efef2347a2a0bcaf5a494d07
Carlos Llamacho <llamacholancho@gmail.com>
2014-03-23 13:43:32 -0400

    Initial commit for Asana OpeneERP Connector. Added a .gitignore for easier tracking and defined an openerp module with __openerp__.py.

---
+++ __openerp__.py
@@ -0,0 +1,19 @@
+#-*- coding: utf-8 -*-
+
+{'name': 'Asana Openerp Connector',
+ 'author': 'Carlos Llamacho',
+ 'version': '0.1',
+ 'category': 'Project Management',
+ 'description': """
+ Does your company uses OpenERP for tracking tasks and teams but you wish something more current were in place?
+ Well, this module will allow your team to use Asana as your preferred tool for tracking tasks and still report
+ to the company using Openerp.
+
+ Just set it up with your account and all your task, projects, and teams will be imported automatically in Openerp project
+ management module for your convenience.""",
+ 'depends':['base',
+            'project'],
+ 'init_xml':[],
+ 'installable': True,
+ 'auto': False}
+

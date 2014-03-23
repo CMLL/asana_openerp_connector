46d7257a28ff381bad71c6b3c32228e417d49643
Carlos Llamacho <llamacholancho@gmail.com>
2014-03-23 19:39:37 -0400

    Fixed syntax error at module declaration.

--- __openerp__.py
+++ __openerp__.py
@@ -13,7 +13,7 @@
  management module for your convenience.""",
  'depends':['base',
             'project'],
- 'init_xml':['asana_connector_view.xml'],
+ 'data':['asana_connector_view.xml'],
  'installable': True,
  'auto': False}
 

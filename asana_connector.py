b465c24c4bbe782284558622d74feeab7f7e16ad
Carlos Llamacho <llamacholancho@gmail.com>
2014-03-24 15:27:42 -0400

    Added fields to populate when making a success connection. Developed five new test cases for checking state of connection and retrieved fields upon succesful connect.

--- asana_connector.py
+++ asana_connector.py
@@ -2,6 +2,7 @@
 
 from openerp.osv import orm, fields
 from asana import asana
+from asana.asana import AsanaException
 
 
 class AsanaConnector(orm.Model):
@@ -10,10 +11,12 @@ class AsanaConnector(orm.Model):
     _auto = True
 
     _columns = {
-        'name': fields.char('User Name', size=32, help='Your Asana username.', required=True),
+        'name': fields.char('User Name', size=32, help='Your Asana username.', readonly=True),
         'api_key': fields.char('API Key', size=32, required=True, help="""Your Asana Account API key,
                                you can generate this key in your settings page of Asana."""),
         'state': fields.selection((('draft', 'Draft'), ('connected', 'Connected')), 'State'),
+        'email': fields.char('Email', size=32, help='Your Asana email', readonly=True),
+        'asana_id': fields.char('Asana Id', size=32, help='Your Asana user id', readonly=True)
     }
 
     _defaults = {
@@ -23,10 +26,18 @@ class AsanaConnector(orm.Model):
     def connect(self, cr, uid, id, context=None):
         """Perform the connection between Openerp and Asana using
         the api_key parameter."""
-        import ipdb; ipdb.set_trace()  # XXX BREAKPOINT
-        user = self.browse(cr, uid, id, context)
-        connection = asana.AsanaAPI(user.api_key)
-        return True
+        for user in self.browse(cr, uid, id, context):
+            connection = asana.AsanaAPI(user.api_key)
+            try:
+                user_info = connection.user_info()
+                self.write(cr, uid, id, {'state': 'connected',
+                                        'email': user_info.get('email'),
+                                        'asana_id': str(user_info.get('id')),
+                                        'name': user_info.get('name')}, context)
+                return True
+            except AsanaException as e:
+                raise orm.except_orm('Error', e.message)
+
 
 
 
--- tests/test_connector.py
+++ tests/test_connector.py
@@ -1,6 +1,7 @@
 #-*- coding: utf-8 -*-
 from openerp.tests import common
 from openerp.osv import orm
+from asana.asana import AsanaAPI
 
 #API Key
 #TODO Change this api key for a dummy account one.
@@ -22,13 +23,50 @@ class TestAsanaConnector(common.TransactionCase):
         self.connection_id = self.connector_obj.create(cr, uid, {'name': 'Dummy account',
                                                                  'api_key' : AKEY,
                                                                  'state': 'draft'})
+        #Also create a record with bad information for negative cases.
+        self.bad_connection_id = self.connector_obj.create(cr, uid, {'name': 'Bad account',
+                                                                     'api_key': '1',
+                                                                     'state': 'draft'})
+        self.testConnection = AsanaAPI('1qBeMGMB.KaHjfA3jwhoficieElDJDqh')
 
     def testConnectionParameters(self):
         """Check the parameters for a success connection."""
         cr, uid = self.cr, self.uid
-
         response = self.connector_obj.connect(cr, uid, self.connection_id)
         self.assertTrue(response)
 
+    def testConnectionState(self):
+        """Check that if the connection is a succes the state of the object changes."""
+        cr, uid = self.cr, self.uid
+        response = self.connector_obj.connect(cr, uid, self.connection_id)
+        connector_state = self.connector_obj.browse(cr, uid, self.connection_id).state
+        self.assertEqual(connector_state, 'connected')
+
+    def testConnectionBadParameters(self):
+        """Check for exception if an error in Api key."""
+        cr, uid = self.cr, self.uid
+        self.assertRaises(orm.except_orm, self.connector_obj.connect, cr, uid, self.bad_connection_id)
+
+    def testConnectionRetrievesUserInfoEmail(self):
+        """Check that when connected the user email is retrieved."""
+        cr, uid = self.cr, self.uid
+        response = self.connector_obj.connect(cr, uid, self.connection_id)
+        connector_email = self.connector_obj.browse(cr, uid, self.connection_id).email
+        self.assertEqual(connector_email, self.testConnection.user_info().get('email'))
+
+    def testConnectionRetrievesUserInfoId(self):
+        """Check that when connected the user id is retrieved."""
+        cr, uid = self.cr, self.uid
+        response = self.connector_obj.connect(cr, uid, self.connection_id)
+        connector_id = self.connector_obj.browse(cr, uid, self.connection_id).asana_id
+        #OpenERP Orm retrieves integer values as string, so parsing is required for test to pass.
+        self.assertEqual(int(connector_id), self.testConnection.user_info().get('id'))
+
+    def testConnectionRetrievesUserName(self):
+        """Check that when connected the user name is retrieved."""
+        cr, uid = self.cr, self.uid
+        response = self.connector_obj.connect(cr, uid, self.connection_id)
+        connector_name = self.connector_obj.browse(cr, uid, self.connection_id).name
+        self.assertEqual(connector_name, self.testConnection.user_info().get('name'))
 
 
